77c24c7d3fe048dfc0d9c9c417ac631dad815812
A <arseniy.smirnov@quantori.com>
2023-05-19 20:13:33 +0100

    Optimized Cookie Parsing; Removed dependendencies

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# You need to set up your cookie inside "main.py"
-IT'S SO FUCKING UNSAFE TO STORE EM LIKE I DID, I JUST WANT TO STOP FOR NOW, OKAY?
- 1. Open https://www.hoyolab.com/home
- 2. Execute document.cookie
+# HoyoLogin
 
-# Fix your python locatoins
-# whatever idc cya
+HoyoLogin is a straightforward Python script for claiming rewards from Hoyoverse games. You don't need to have excessive dependencies, read lines of spaghetti code to find if there is some sneaky shit hidden or over-engineering pseudo "user-friendly" way to interact.
+
+## Requirements
+
+- Python
+- requests library (can be installed via `pip install requests`)
+
+## Getting Started
+
+1. Clone the repository to your local machine.
+2. Create a file named `tokens.json` in the project directory.
+3. Open the [Hoyolab website](https://www.hoyolab.com/home) in any browser.
+4. In the browser console, execute the command `document.cookie` to retrieve the cookie value. Keep this value safe and do not share it with others.
+5. Paste the cookie value inside the `tokens.json` file as shown below:
+
+```json
+{
+    "RandomName": "cookie_value_here",
+    "User2": "if you want to add another user"
+}
+```
+6. (Optional) Edit the `login.bat` file:
+   - Update the first line to specify the directory of the HoyoLogin project.
+   - Update the second line to provide the full path to the Python interpreter and `main.py`.
+7. (Optional) If you want to schedule the task, adjust the properties inside the `schedule_task.ps1` file according to your preferences.
+
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
 import requests
-from requests.exceptions import HTTPError
-import http.cookies
-
 
 # Constants
 with open('tokens.json') as file:
@@ -10,14 +7,7 @@ with open('tokens.json') as file:
 GI_URL = "https://sg-hk4e-api.hoyolab.com/event/sol/sign?act_id=e202102251931481"
 HSR_URL = "https://sg-public-api.hoyolab.com/event/luna/os/sign?act_id=e202303301540311"
 
-def parseCookie(USERS_COOKIES):
-    parsed_cookies = {}
-    for user, cookie_string in USERS_COOKIES.items():
-        cookie = http.cookies.SimpleCookie()
-        cookie.load(cookie_string)
-        parsed_cookies[user] = {name: cookie[name].value for name in cookie}
-    return parsed_cookies
-
+# Debug function to test access
 def getAccountInfo(cookies):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
@@ -28,10 +18,11 @@ def getAccountInfo(cookies):
     try:
         response = requests.get("https://api-account-os.hoyolab.com/auth/api/getUserAccountInfoByLToken", headers=headers, cookies=cookies)
         return response.json()
-    except Exception as e:
-        return e
+    except requests.exceptions.RequestException as e:
+        return str(e)
 
-def ClaimReward(cookies, URI):
+# The only function you need
+def claimReward(cookies, URI):
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Accept-Language": "ja-JP,ja;q=0.5",
@@ -39,25 +30,21 @@ def ClaimReward(cookies, URI):
         "Connection": "keep-alive"
     }
 
-    params = (("lang", "en-us"),)
-
     try:
-        response = requests.post(URI, headers=headers, params=params, cookies=cookies)
+        response = requests.post(URI, headers=headers, cookies=cookies)
         return response.json()
-    except Exception as e:
-        return e
+    except requests.exceptions.RequestException as e:
+        return str(e)
 
 if __name__ == "__main__":
-    parsed_cookies = parseCookie(USERS_COOKIES)
-    for user, cookie in parsed_cookies.items():
+    for user, cookie_string in USERS_COOKIES.items():
         print(f"Processing user: {user}")
-        result = getAccountInfo(cookie)
-        print(result)
+        cookies = requests.utils.cookiejar_from_dict({user: cookie_string})
 
         print("Claiming rewards from GI")
-        result = ClaimReward(cookie, GI_URL)
+        result = claimReward(cookies, GI_URL)
         print(result)
 
         print("Claiming rewards from HSR")
-        result = ClaimReward(cookie, HSR_URL)
+        result = claimReward(cookies, HSR_URL)
         print(result)
